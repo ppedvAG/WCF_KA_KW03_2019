f281f014fb0e0bae7ab579b81d3772cec4b38339
André <andrerrr@outlook.de>
2019-01-17 12:17:53 +0100

    Moooose Server gut

---
+++ ppedv.TalkingMoose/ppedv.TalkingMoose.Server/Program.cs
@@ -0,0 +1,32 @@
+﻿using ppedv.TalkingMoose.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ppedv.TalkingMoose.Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*** Takling MOOOOOOSE Server v0.1  ***");
+
+            var tcp = new NetTcpBinding();
+
+            var host = new ServiceHost(typeof(Server));
+            host.AddServiceEndpoint(typeof(IServer), tcp, "net.tcp://localhost:1");
+
+            host.Open();
+            Console.WriteLine("Server gestartet");
+            Console.ReadLine();
+            host.Close();
+            Console.WriteLine("Server beendet");
+
+            Console.WriteLine("Ende");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ppedv.TalkingMoose/ppedv.TalkingMoose.Server/Server.cs
@@ -0,0 +1,34 @@
+﻿using ppedv.TalkingMoose.Contracts;
+using System;
+using System.IO;
+
+namespace ppedv.TalkingMoose.Server
+{
+    public class Server : IServer
+    {
+        public void Login(string name)
+        {
+            Console.WriteLine($"Login: {name}");
+        }
+
+        public void Logout()
+        {
+            Console.WriteLine($"Logout");
+        }
+
+        public void SendFile(Stream file)
+        {
+            Console.WriteLine($"SendFile");
+        }
+
+        public void SendPic(Stream pic)
+        {
+            Console.WriteLine($"SendPic");
+        }
+
+        public void SendText(string txt)
+        {
+            Console.WriteLine($"SendText: {txt}");
+        }
+    }
+}
