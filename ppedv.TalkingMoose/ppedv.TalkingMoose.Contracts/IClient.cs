61f2d264870fd831729074a427a760d675a184e0
André <andrerrr@outlook.de>
2019-01-17 12:07:57 +0100

    Contracts für den Mega Chat

---
+++ ppedv.TalkingMoose/ppedv.TalkingMoose.Contracts/IClient.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.ServiceModel;
+
+namespace ppedv.TalkingMoose.Contracts
+{
+    [ServiceContract]
+    public interface IClient
+    {
+        [OperationContract(IsOneWay = true)]
+        void ShowText(string txt);
+
+        [OperationContract(IsOneWay = true)]
+        void ShowPic(Stream pic);
+
+        [OperationContract(IsOneWay = true)]
+        void ShowFile(Stream file);
+
+        [OperationContract(IsOneWay = true)]
+        void ShowUserlist(IEnumerable<string> users);
+
+        [OperationContract(IsOneWay = true)]
+        void LoginOk();
+
+        [OperationContract(IsOneWay = true)]
+        void LoginFailed(string txt);
+    }
+}
---
+++ ppedv.TalkingMoose/ppedv.TalkingMoose.Contracts/IServer.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using System.ServiceModel;
+
+namespace ppedv.TalkingMoose.Contracts
+{
+    [ServiceContract(CallbackContract = (typeof(IClient)))]
+    public interface IServer
+    {
+        [OperationContract(IsOneWay = true)]
+        void SendText(string txt);
+
+        [OperationContract(IsOneWay = true)]
+        void SendPic(Stream pic);
+
+        [OperationContract(IsOneWay = true)]
+        void SendFile(Stream file);
+
+        [OperationContract(IsOneWay = true)]
+        void Login(string name);
+
+        [OperationContract(IsOneWay = true)]
+        void Logout();
+    }
+}
